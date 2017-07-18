229bbb7e5c03cb6e7a1ff86a8a064fbb9ad5015f
Sergey-Akmalov <akmolgt@gmail.com>
2017-07-18 20:46:54 +0300

    Optimize webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,29 @@
-let loaders = require('./webpack.config.loaders')();
-let path = require('path');
+const path = require('path');
+const loaders = require('./webpack.config.loaders')();
+const merge = require('webpack-merge');
+const devserver = require('./webpack/devserver');
 
-module.exports = {
+const common = {
     entry: [
         'react-hot-loader/patch',
         path.join(__dirname, 'src', 'index.js')],
     output: {
         filename: 'bundle.js',
-        path: path.resolve('public'),
+        path: path.resolve(__dirname, 'public'),
     },
     module: {
         loaders
+    }
+};
+
+module.exports = function(env) {
+   if (env === 'production'){
+       return common;
+   }
+    if (env === 'development'){
+        return merge([
+            common,
+            devserver()
+        ])
     }
 };
